f7ec9147643df21c70b8ea8fd00c56182eea0ac1
mohitviionn-star <mohit.viionn@gmail.com>
2026-02-09 17:38:53 +0530

    remove toast message

--- src/components/HealthCheck.tsx
+++ src/components/HealthCheck.tsx
@@ -5,20 +5,20 @@ import { useGetHealthStatusQuery } from "@/rtk/api/healthApi";
 const HealthCheck = () => {
   const { isSuccess, isError } = useGetHealthStatusQuery();
 
-  useEffect(() => {
-    if (isSuccess) {
-      toast.success("Cure Cottage Connected", {
-        description: "Backend is up and running!",
-        duration: 3000,
-      });
-    }
-    if (isError) {
-      toast.error("Connection Failed", {
-        description: "Unable to connect to backend server",
-        duration: 4000,
-      });
-    }
-  }, [isSuccess, isError]);
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     toast.success("Cure Cottage Connected", {
+  //       description: "Backend is up and running!",
+  //       duration: 3000,
+  //     });
+  //   }
+  //   if (isError) {
+  //     toast.error("Connection Failed", {
+  //       description: "Unable to connect to backend server",
+  //       duration: 4000,
+  //     });
+  //   }
+  // }, [isSuccess, isError]);
 
   return null;
 };
